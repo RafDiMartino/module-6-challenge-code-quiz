5726a83bd210151d3e2169fcdfa3c7a337038712
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-01-15 18:45:44 +0000

    adding logic to start the game on click with the timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,20 @@
 //Variables
+const questionsDiv = document.getElementById("questions")
+const questionTitle = document.getElementById('question-title');
+const choices = document.getElementById('choices');
+const startScreen = document.getElementById("start-screen");
 const startBtn = document.getElementById("start");
+const endScreen = document.getElementById("end-screen");
+const feedback = document.getElementById("feedback")
+const finalScore = document.getElementById("final-score")
+const initials = document.getElementById("initials")
+const submitInitials = document.getElementById("submit")
+const highscoresOl = document.getElementById("highscores");
+const rightAnsAudio = new Audio("assets/sfx/correct.wav")
+const wrongAnsAudio = new Audio("assets/sfx/incorrect.wav")
+
 let timer = document.getElementById("time");
 let countdown = 60;
-var questionIndex = 0;
 
 // Function that starts the game
 function startGame(){
@@ -10,8 +22,9 @@ function startGame(){
         console.log("clicked")
         timer.textContent = countdown
         countdownTimer()
+        displayQuestion()
     })
-    
+
 }
 startGame()
 
@@ -20,15 +33,16 @@ function countdownTimer(){
     let timeInterval = setInterval( () => {
         countdown--;
         timer.textContent = countdown
-        if (countdown === 0) {
+        if (countdown <= 0) {
             clearInterval(timeInterval)
             console.log("ended")
-            return
+            questionsDiv.classList.add("hide")
+            endScreen.classList.remove("hide")
+            finalScore.textContent = `${totalScore}`
+            timer.textContent = 0;
         }
     }, 1000)
+    scores()
 }
 
-// function to update the timer on wrong or right answer
-function updateTimer(){
 
-}
